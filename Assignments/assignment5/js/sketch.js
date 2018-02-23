fa43865bd2f1918eecb5e86ee3b07677d927aa87
--deeksha13kalra <dsk2157@columbia.edu>
2018-02-23 01:29:05 -0500

    re2assignment5

--- Assignments/assignment5/js/sketch.js
+++ Assignments/assignment5/js/sketch.js
@@ -1,12 +1,13 @@
 // ** Global variables ** //
 var yearsVsFloors;
 var topY = 50;
-var bottomY = 500;
+var bottomY = 800;
 var leftX = 50;
-var rightX = 700;
+var rightX = 1000;
 var textLeft = 30;
+var delay;
 
-//** Preload function ** //
+// ** Preload function ** //
 function preload(){
   yearsVsFloors = loadTable('./data/yearsVsFloors.csv', 'csv', 'header');
   console.log('Done loading table...');
@@ -14,53 +15,75 @@ function preload(){
 
 // ** Setup function ** //
 function setup(){
-  createCanvas(800, 800);
+  createCanvas(1100, 900);
   textSize(12);
   textFont('Roboto');
   console.log('Setup complete...');
-  // print(moviesTable.getRowCount() + ' rows loaded...');
-  // print(moviesTable.getColumnCount() + ' columns loaded...');
+  console.log(yearsVsFloors.getRowCount() + ' rows loaded...');
+  console.log(yearsVsFloors.getColumnCount() + ' columns loaded...');
+  colorMode(HSB, 100);
   noLoop();
-  
-}
 
-function drawGraph(){
   background(255);
-  fill(0);
+  // fill(255);
+  // Draw horizontal lines and the y axis
   for (var i = 0; i < 24; i++) {
     noStroke();
     textAlign(RIGHT, CENTER);
     text(round(map(i, 0, 23, 1900, 2015)), textLeft, map(i, 0, 23, bottomY, topY));
-    stroke(200);
+    stroke(80);
     line(textLeft + 10, map(i, 0, 23, bottomY, topY), rightX + 10, map(i, 0, 23, bottomY, topY));
+
   }
   noStroke();
+  // Draw vertical lines and the x axis
   for (var i = 0; i < 15; i++) {
     textAlign(CENTER, TOP);
     text(round(map(i, 0, 14, 0, 70)), map(i, 0, 14, leftX, rightX), bottomY + 5);
-    line(map(i, 0, 14, leftX, rightX), textLeft, map(i, 0, 14, leftX, rightX), rightX);
-  }
+    stroke(80);
+    line(map(i, 0, 14, 40, 1010), topY , map(i, 0, 14, 40, 1010), bottomY);
+  }  
+}
 
-for (var i = 0; i < yearsVsFloors.getRowCount(); i++) {
-    var numfloor = parseInt(yearsVsFloors.getString(i, 'NumFloors'));
-    var yearbuilt = parseInt(yearsVsFloors.getString(i, 'YearBuilt'));
-    if(yearbuilt < 1900 || numfloor>70) {
+
+function drawGraph(column){
+
+
+  for (var i = 0; i < yearsVsFloors.getRowCount(); i++) {
+    // Read per row the numfloor and yearbuilt
+    var numfloor = parseFloat(yearsVsFloors.getString(i, 'NumFloors'));
+    var year;
+    if (column==0) {
+      year = parseFloat(yearsVsFloors.getString(i, 'YearBuilt'));
+      fill(100,250,212);
+    }
+    if (column==1) {
+      year = parseFloat(yearsVsFloors.getString(i, 'YearAlter1'));
+      fill(50,112,77);
+    }
+    if (column==2) {
+      year = parseFloat(yearsVsFloors.getString(i, 'YearAlter2'));
+      fill(150,123,24);
+    }
+
+    // If year is less than 1900 or numfloor is greater is 70
+    if ((year < 1900) || (numfloor > 70) || (year > 2015))  {
       continue;
     }
-    console.log(numfloor, yearbuilt);
+    // map each floor between leftX and rightX
     numfloor = map(numfloor, 0, 70, leftX, rightX);
-    yearbuilt = map(yearbuilt, 1900, 2015, bottomY, topY);
-    ellipse(numfloor, yearbuilt, 2, 2);
+    // map each yearbuilt between bottomY and topY
+    year = map(year, 1900, 2015, bottomY, topY);
+    noStroke();    
+    ellipse(numfloor, year, 4, 4);
   }
-  //   var date = table.getString(i, 'release_date');
-  //   var year = parseInt(date);
-  //   var yearPosition = map(year, 1916, 2017, leftX, rightX);
-  //   var scorePosition = map(table.getNum(i, 'vote_average'), 0, 10, bottomY, topY);
-  //   ellipse(yearPosition, scorePosition, 3, 3);
-  // }
+
 }
 
 // ** Draw function ** //
 function draw(){
-  drawGraph();
+    drawGraph(0);
+  drawGraph(1);
+  drawGraph(2);
+  
 }
