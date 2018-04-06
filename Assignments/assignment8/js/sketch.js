1659ad0f22526f6cf078947444654069017f48e0
--deeksha13kalra <dsk2157@columbia.edu>
2018-04-05 23:11:03 -0400

    weatherApp

--- Assignments/assignment8/js/sketch.js
+++ Assignments/assignment8/js/sketch.js
@@ -3,65 +3,66 @@ var apiKey = '&APPID=078d6c73ac474215a151b41028f6a4b9';
 var api = 'http://api.openweathermap.org/data/2.5/weather?q='
 var units = '&units=metric';
 var weatherData;
-var image;
+var cityInput;
 
 // * Setup Function * //
-function setup(){
-  var canvas = createCanvas(800, 200)
-    // jumbo-canvas is a string
-    canvas.parent('jumbo-canvas')
-    // background(255, 0, 200)
-}
-var button = select('#submit');
-button.mousePressed(weatherAsk);
+function setup() {
+    var canvas = createCanvas(400, 200);
+    background(250);
+    canvas.parent('jumbo-canvas');
 
-cityInput = select('#city');
-noLoop();
- }
-
-/* function mousePressed(cityInput){
- 	remove();
- }
-*/
-function weatherAsk(){
-  city = cityInput.value();
-	var request = api + city  + apiKey + units;
-  loadJSON(request, gotData);
+    var button = select('#submit');
+    cityInput = select('#city');
+    button.mousePressed(weatherAsk);
+    textSize(18);
+    noLoop();
 }
 
-function gotData(data){
-	weatherData = data;
-	description = weatherData.weather[0].description;
-  temperature = weatherData.main.temp;
-  humidity = weatherData.main.humidity;
-  pressure = weatherData.main.pressure;
-  print(weatherData);
+
+function weatherAsk() {
+    city = cityInput.value();
+    if (city == "Banana City") {
+        alert("Psst... type a real city!");
+        return;
+    }
+    var request = api + city + apiKey + units;
+    loadJSON(request, gotData);
 }
 
-function draw(){
- background(255);
+function gotData(data) {
+    weatherData = data;
+    description = weatherData.weather[0].description;
+    temperature = Math.round (weatherData.main.temp);
+    humidity = weatherData.main.humidity;
+    pressure = weatherData.main.pressure;
+    print(weatherData);
 
-  if (weatherData){
-    text('The current weather for ' + city + ' is:', 50, 50);
-    text(description, 80, 70);
-    text(temperature + ' degrees C', 80, 90);
-    text(humidity + '% humidity', 80, 110);
-    text(pressure + ' hPa (pressure)', 80, 130);
+    if (weatherData) {
+        // clear current city and redraw
+        clear();
+        background(255);
+        text('The current weather for ' + city + ' is:', 50, 50);
+         textSize(10);
+        text(description, 80, 70);
+ textSize(55);
+        text(temperature + '° C', 80, 90);
+         textSize(10);
+        text(humidity + '% humidity', 80, 110);
+        text(pressure + ' hPa (pressure)', 80, 130);
 
-    if(description=='light rain') {
-      image = "images/rain.jpg"
+        if (description == 'light rain') {
+            imagesrc = "images/rain.jpg"
+        } else if (description == 'clear sky') {
+            imagesrc = "images/snow.jpg"
+        } else {
+            imagesrc = "images/sunny.png"
+        }
+
+        document.getElementById('headerImg').src = imagesrc;
+
     }
+}
 
-    else if (description == 'clear sky') {
-      image = "images/snow.jpg"
-    }
-
-    else {
-     image = "images/sunny.png" 
-    }
-
-    document.getElementById('headerImg').src = image;
-
-  }
+function draw() {
 
 }
